fee78acb8afb2bfc821c5b73752181b26472c4aa
omesh-dhanushka <omesh.d@aeturnum.com>
2020-08-16 20:42:53 +0530

    added new changes

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <iostream>
+#include <memory>
 
 using namespace std;
 
 int main() {
     cout<< "Hello World!!" <<endl;
+
+    unique_ptr<int> number1 = make_unique<int> (10);
+
     return 0;
 }
